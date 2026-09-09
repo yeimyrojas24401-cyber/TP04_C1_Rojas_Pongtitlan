f9dd3605c6100e2e31034fb26b9fef3c1f53210f
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-09 15:34:08 -0600

    GeneralPowerUpSettings

--- Assets/Scripts/CollisionHandle.cs
+++ Assets/Scripts/CollisionHandle.cs
@@ -7,12 +7,19 @@ public class CollisionHandle : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         spriteRenderer.color = Color.gray;
     }
+    //entre funcion y funcion se puede poner un espacio en blanco para que se vea mejor
     private void OnCollisionExit2D(Collision2D collision)
     {
         spriteRenderer.color = Color.white;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Entered");
     }
 }
