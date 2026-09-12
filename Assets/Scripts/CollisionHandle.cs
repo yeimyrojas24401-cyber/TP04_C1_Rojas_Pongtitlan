20fabb21f2beaa42ac344ebf40823c08aed45ed5
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-11 22:15:14 -0600

    CollisionHandleUpdates

--- Assets/Scripts/CollisionHandle.cs
+++ Assets/Scripts/CollisionHandle.cs
@@ -4,7 +4,10 @@ public class CollisionHandle : MonoBehaviour
 {
     [SerializeField] private PlayerVisualController visual;
     private Rigidbody2D rb;
-
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (visual.SpriteRenderer == null) return;
@@ -14,12 +17,15 @@ public class CollisionHandle : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Ball"))
             visual.SpriteRenderer.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.8f, 1f);
+        rb.linearVelocity = Vector2.zero; // esto nos permite que cuando la pelota vaya a pegarle al 
+                                          //se cancela la velocida y ya no empuja al jugador
+
     }
 
     //entre funcion y funcion se puede poner un espacio en blanco para que se vea mejor
     private void OnCollisionExit2D(Collision2D collision)
     {
-    if (visual.SpriteRenderer != null)
+        if (visual.SpriteRenderer != null)
         {
             visual.SpriteRenderer.color = Color.white;
         }
