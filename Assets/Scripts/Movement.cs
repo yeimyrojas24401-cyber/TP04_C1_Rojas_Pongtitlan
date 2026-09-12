1fd89a7099c6159ca483fa90b2f0cea7461d03aa
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-11 21:43:41 -0600

    UpdateMovementLimits

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,40 +10,66 @@ public class Movement : MonoBehaviour
     [Header("Debug: ")]
     [SerializeField] private float moveSpeedPlayer = 1000.0f;
 
+    [Header("Field Limits")]
+    [SerializeField] private float minX = -8f;
+    [SerializeField] private float center = 0f;
+    [SerializeField] private float maxX = 8f;
+    [SerializeField] private bool isLeftSide = true; // check this on P1, uncheck on P2
 
     private Rigidbody2D rb;
-    
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
-    private void Start ()
+
+    private void Start()
     {
         moveSpeedPlayer = data.speed;
     }
 
     private void FixedUpdate()
     {
-       if (isContinuousMovement)
+        if (isContinuousMovement)
         {
-            // Movimiento continuo con fisicas
             if (Input.GetKey(data.moveUp))
-            {
-                rb.AddForce(new Vector3(0, moveSpeedPlayer * Time.fixedDeltaTime));
-            }
+                rb.AddForce(new Vector2(0, moveSpeedPlayer * Time.fixedDeltaTime));
             if (Input.GetKey(data.moveRight))
-            {
-                rb.AddForce(new Vector3(moveSpeedPlayer * Time.fixedDeltaTime, 0));
-            }
+                rb.AddForce(new Vector2(moveSpeedPlayer * Time.fixedDeltaTime, 0));
             if (Input.GetKey(data.moveDown))
-            {
-                rb.AddForce(new Vector3(0, -moveSpeedPlayer * Time.fixedDeltaTime));
-            }
+                rb.AddForce(new Vector2(0, -moveSpeedPlayer * Time.fixedDeltaTime));
             if (Input.GetKey(data.moveLeft))
-            {
-                rb.AddForce(new Vector3(-moveSpeedPlayer * Time.fixedDeltaTime, 0));
-            }
+                rb.AddForce(new Vector2(-moveSpeedPlayer * Time.fixedDeltaTime, 0));
         }
+
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        Vector2 clampedPos = rb.position;
+        Vector2 velocity = rb.linearVelocity;
+
+        float clampedX;
+
+        if (isLeftSide)
+        {
+            clampedX = Mathf.Clamp(clampedPos.x, minX, center);
+        }
+        else
+        {
+            clampedX = Mathf.Clamp(clampedPos.x, center, maxX);
+        }
+
+        // if clamping changed the x position, we hit a boundary -> kill x velocity
+        if (!Mathf.Approximately(clampedX, clampedPos.x))
+        {
+            velocity.x = 0f;
+        }
+
+        clampedPos.x = clampedX;
+
+        rb.position = clampedPos;
+        rb.linearVelocity = velocity;
     }
 }
