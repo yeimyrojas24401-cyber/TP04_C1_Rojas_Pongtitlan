a30597ce2896fa8eb32a997dcb17a2bfd9d5521f
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-14 00:01:50 -0600

    Update PlayerDataSo

--- Assets/Scripts/Data/PlayerDataSo.cs
+++ Assets/Scripts/Data/PlayerDataSo.cs
@@ -12,6 +12,21 @@ public class PlayerDataSo : ScriptableObject
     [Header("SpeedSettings")]
     public float[] speedOptions = { 500f, 1000f, 2000f }; // opciones de speed
     [NonSerialized] public int speedIndex = 1; //asi lo ponemos en default en la posicion 1 = 1000
+
+    [Header("VisualSettings")]
+    [NonSerialized] public int variantIndex = 0;
+    public GameObject [] variantPrefabs = new GameObject[3]; // aqui hice un array de 3 para la informacion de sus sprites
+
+    [Header("ColorSettings")]
+    public Color[] colorOptions = { Color.white, Color.green, Color.blue };
+    [NonSerialized] public int colorIndex = 0;
+
+    //Events
+    public event Action<float> OnSpeedChanged; // este es un evento al que otros pueden llamar especificamente lo cree para los titles y los slider
+    public event Action<int> OnVariantChanged;
+    public event Action<Color> OnColorChanged;
+
+    // Properties
     public float speed // este float lo pense para devolver el valor actual FLOAT segun el indice de opciones
                        // pensemos que actualmente en nuestro array guardamos tres opciones
                        // y cada una vale algo. Cuando movement mande a llamar a este float va a buscar en ese array
@@ -30,34 +45,6 @@ public class PlayerDataSo : ScriptableObject
             }
         }
     }
-    public event Action<float> OnSpeedChanged; // este es un evento al que otros pueden llamar especificamente lo cree para los titles y los slider
-    public void SetSpeedIndex(int index) //metodo para cambiar el speedIndex
-    {
-        if (speedOptions == null || speedOptions.Length == 0) return; // si no tengo opciones en speed no hagas nada
-        speedIndex = Mathf.Clamp(index, 0, speedOptions.Length - 1); // guarda el indice y actualiza pero no permitas que se salga de el numero de opciones que me dieron (esto es para el slider)
-        OnSpeedChanged?.Invoke(speed); // ejecuta el evento OnSpeedChanged ahora
-    }
-
-    [Header("VisualSettings")]
-    public GameObject [] variantPrefabs = new GameObject[3]; // aqui hice un array de 3 para la informacion de sus sprites
-
-    [NonSerialized] public int variantIndex = 0;
-
-    public GameObject CurrentVariant => (variantPrefabs != null && variantPrefabs.Length > 0)
-        ? variantPrefabs[Mathf.Clamp(variantIndex, 0, variantPrefabs.Length - 1)]
-        : null;
-
-    public event Action<int> OnVariantChanged;
-    public void SetVariantIndex(int index)
-    {
-        if (variantPrefabs == null || variantPrefabs.Length == 0) return;
-        variantIndex = Mathf.Clamp(index, 0, variantPrefabs.Length - 1);
-        OnVariantChanged?.Invoke(variantIndex);
-    }
-    [Header("ColorSettings")]
-    public Color[] colorOptions = { Color.white, Color.green, Color.blue };
-    [NonSerialized] public int colorIndex = 0;
-
     public Color color
     {
         get
@@ -67,15 +54,36 @@ public class PlayerDataSo : ScriptableObject
             return Color.white;
         }
     }
+    public GameObject CurrentVariant => (variantPrefabs != null && variantPrefabs.Length > 0)
+        ? variantPrefabs[Mathf.Clamp(variantIndex, 0, variantPrefabs.Length - 1)]
+        : null;
 
-    public event Action<Color> OnColorChanged;
-
+    //methods
+    public void SetSpeedIndex(int index) //metodo para cambiar el speedIndex
+    {
+        if (speedOptions == null || speedOptions.Length == 0) return; // si no tengo opciones en speed no hagas nada
+        speedIndex = Mathf.Clamp(index, 0, speedOptions.Length - 1); // guarda el indice y actualiza pero no permitas que se salga de el numero de opciones que me dieron (esto es para el slider)
+        OnSpeedChanged?.Invoke(speed); // ejecuta el evento OnSpeedChanged ahora
+    }
+    public void SetVariantIndex(int index)
+    {
+        if (variantPrefabs == null || variantPrefabs.Length == 0) return;
+        variantIndex = Mathf.Clamp(index, 0, variantPrefabs.Length - 1);
+        OnVariantChanged?.Invoke(variantIndex);
+    }
     public void SetColorIndex(int index)
     {
         if (colorOptions == null || colorOptions.Length == 0) return;
         colorIndex = Mathf.Clamp(index, 0, colorOptions.Length - 1);
         OnColorChanged?.Invoke(color);
     }
+
+
+
+
+
+
+
 
 
 }
